f2627d5d4dafef77ceaf1d74998c4d654001ddc0
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-10-17 04:24:25 +0100

    add loadings

--- src/components/BlogContent.jsx
+++ src/components/BlogContent.jsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Image from "next/image";
 import { urlFor } from "@/utils/sanity-utils";
 import { client } from "../../sanity/lib/client";
+import Skeleton from "./Loading/Skeleton";
 
 const BlogContent = () => {
   const [data, setData] = useState([]);
@@ -13,7 +14,6 @@ const BlogContent = () => {
     const query =
       '*[_type == "post"]{ _id, title, mainImage, "authorname": author->name }';
     const posts = await client.fetch(query);
-    console.log(posts);
     setData(posts);
     setLoading(false);
   };
@@ -23,12 +23,16 @@ const BlogContent = () => {
   }, []);
 
   return (
-    <div>
+    <div className=''>
       <h1>blog posts</h1>
       {loading ? (
-        <p>Loading...</p>
+        <div className='grid grid-cols-3 gap-3'>
+          {Array.from({ length: 6 }).map((index) => {
+            return <Skeleton key={index} />;
+          })}
+        </div>
       ) : (
-        <>
+        <div className='grid grid-cols-3'>
           {data.map((post) => {
             const imageSrc = urlFor(post.mainImage)
               .width(1000)
@@ -48,7 +52,7 @@ const BlogContent = () => {
               </div>
             );
           })}
-        </>
+        </div>
       )}
     </div>
   );
---
+++ src/components/Loading/Skeleton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Skeleton = () => {
+  return <div className='w-72 h-72 bg-gray-500 p-3'></div>;
+};
+
+export default Skeleton;
