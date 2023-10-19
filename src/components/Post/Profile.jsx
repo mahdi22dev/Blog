015a89f2b9b9c81a5b13f0bcd3c9539c9dd32d48
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-10-19 23:10:49 +0100

    f

--- src/components/Post/Profile.jsx
+++ src/components/Post/Profile.jsx
@@ -8,7 +8,7 @@ export default function Profile({ post }) {
       <img
         src={urlFor(post.author.image).width(50).height(50).url()}
         alt={post.author.image.alt}
-        className='rounded-[100%] w-11 h-11 '
+        className='rounded-[100%] w-11 h-11'
       />
       <p className='text-xs text-black  dark:text-white font-medium '>
         <Link className='not-prose hover:text-primary transition' href={"/"}>
