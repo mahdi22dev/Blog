7a7af72d265cc163a2d7a62256b8cc850b3b75b4
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-10-24 05:57:45 +0100

    remove new letter

--- src/config/nav-links.js
+++ src/config/nav-links.js
@@ -2,6 +2,5 @@ export const navlinks = [
   { id: 1, title: "Home", link: "/" },
   { id: 2, title: "Blog", link: "/blog" },
   { id: 4, title: "#Tags", link: "/tags" },
-  { id: 5, title: "Newsletter", link: "/letter" },
   { id: 3, title: "About", link: "/about" },
 ];
