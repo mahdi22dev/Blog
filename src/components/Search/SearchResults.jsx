ce4d2c9d1eba9994631ef76ae7faca1e86341a09
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-10-25 02:39:33 +0100

    fix issue

--- src/components/Search/SearchResults.jsx
+++ src/components/Search/SearchResults.jsx
@@ -4,6 +4,7 @@ import { client } from "../../../sanity/lib/client";
 import Skeleton from "../Loading/Skeleton";
 import Featured from "../Home/Featured";
 import { useSearchParams } from "next/navigation";
+import { LiaSearchSolid } from "react-icons/lia";
 
 let start = 0;
 let end = 5;
@@ -55,54 +56,61 @@ const SearchResults = () => {
   }
 
   return (
-    <div className='pb-20'>
-      {loading ? (
-        <div className='grid grid-cols-1 md:grid-cols-2 min-[1440px]:grid-cols-3 w-full h-full max-w-7xl mx-auto'>
-          {Array.from({ length: 6 }).map((index) => {
-            return <Skeleton key={index} />;
-          })}
-        </div>
-      ) : (
-        <div className='grid grid-cols-1 md:grid-cols-2 min-[1440px]:grid-cols-3 w-full h-full max-w-7xl mx-auto'>
-          {data.map((post) => {
-            return (
-              <Featured
-                key={post.slug}
-                post={post}
-                width={""}
-                maxw={""}
-                className={"m-2 w-[90%] min-[1440px]:w-96"}
-              />
-            );
-          })}
-        </div>
-      )}
-      {paginationLoading ? (
-        <div className='grid grid-cols-1 md:grid-cols-2 min-[1440px]:grid-cols-3 w-full h-full max-w-7xl mx-auto'>
-          {Array.from({ length: 6 }).map((index) => {
-            return <Skeleton key={index} />;
-          })}
-        </div>
-      ) : (
-        <div className='flex justify-center items-center w-full absolute bottom-2'>
-          {showMore ? (
-            <button
-              onClick={() => {
-                end = end + 6;
-                start = start + 6;
-                PaginationFetch(start, end);
-              }}
-              href={"/author"}
-              className='mt-3 mx-auto btn_animation text-xs'
-            >
-              <span>Load More</span>
-            </button>
-          ) : (
-            <p>no more posts to show</p>
-          )}
-        </div>
-      )}
-    </div>
+    <>
+      {" "}
+      <p className='text-start ml-7 my-10 text-2xl flex gap-2 items-center justify-start'>
+        <LiaSearchSolid /> Search Results for:
+        <span className='text-primary font-semibold flex gap-2'>{search}</span>
+      </p>
+      <div className='pb-20'>
+        {loading ? (
+          <div className='grid grid-cols-1 md:grid-cols-2 min-[1440px]:grid-cols-3 w-full h-full max-w-7xl mx-auto'>
+            {Array.from({ length: 6 }).map((index) => {
+              return <Skeleton key={index} />;
+            })}
+          </div>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-2 min-[1440px]:grid-cols-3 w-full h-full max-w-7xl mx-auto'>
+            {data.map((post) => {
+              return (
+                <Featured
+                  key={post.slug}
+                  post={post}
+                  width={""}
+                  maxw={""}
+                  className={"m-2 w-[90%] min-[1440px]:w-96"}
+                />
+              );
+            })}
+          </div>
+        )}
+        {paginationLoading ? (
+          <div className='grid grid-cols-1 md:grid-cols-2 min-[1440px]:grid-cols-3 w-full h-full max-w-7xl mx-auto'>
+            {Array.from({ length: 6 }).map((index) => {
+              return <Skeleton key={index} />;
+            })}
+          </div>
+        ) : (
+          <div className='flex justify-center items-center w-full absolute bottom-2'>
+            {showMore ? (
+              <button
+                onClick={() => {
+                  end = end + 6;
+                  start = start + 6;
+                  PaginationFetch(start, end);
+                }}
+                href={"/author"}
+                className='mt-3 mx-auto btn_animation text-xs'
+              >
+                <span>Load More</span>
+              </button>
+            ) : (
+              <p>no more posts to show</p>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
